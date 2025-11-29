288bb8fb90d2175889550eb30e8dfbc421a57a02
Wasi Master <arianmollik323@gmail.com>
2025-11-30 01:12:02 +0600

    Auto commit via web UI @ 2025-11-30T01:12:02.652738

--- t_shirt.py
+++ t_shirt.py
@@ -2,5 +2,13 @@ import json
 import collections
 with open('./registrants.json', 'r') as file:
     registrations = json.load(file)
-tshirt_sizes = collections.Counter(i.get('tshirt_size') for i in registrations if i['gender'] == "Female")
-print(tshirt_sizes)
+tshirt_sizes_female = collections.Counter(i.get('tshirt_size') for i in registrations if i['gender'] == "Female")
+tshirt_sizes_male = collections.Counter(i.get('tshirt_size') for i in registrations if i['gender'] == "Male")
+print("Meyeder Shirt size:")
+for x,y in tshirt_sizes_female.items():
+    print(f"{y} ta {x}")
+print("Total shirts:", sum(tshirt_sizes_female.values()))
+print("\nCheleder Genji size:")
+for x,y in tshirt_sizes_male.items():
+    print(f"{y} ta {x}")
+print("Total t-shirts:", sum(tshirt_sizes_male.values()))
